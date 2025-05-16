db67321813c47ffd6a824e714d3014357b7add30
ARYAN x SHOAIB <shoaib910385@gmail.com>
2025-05-16 21:23:43 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ API_ID = int(getenv("API_ID", "6435225")) #optional
 API_HASH = getenv("API_HASH", "") #optional
 
 SUDO_USERS = list(map(int, getenv("SUDO_USERS", "").split()))
-OWNER_ID = int(getenv("OWNER_ID","6762113050"))
+OWNER_ID = int(getenv("OWNER_ID","7659846392"))
 MONGO_URL = getenv("MONGO_URL")
 BOT_TOKEN = getenv("BOT_TOKEN", "")
 ALIVE_PIC = getenv("ALIVE_PIC", 'https://files.catbox.moe/rz44me.jpg')
@@ -21,7 +21,7 @@ GIT_TOKEN = getenv("GIT_TOKEN") #personal access token
 REPO_URL = getenv("REPO_URL", "https://github.com/shoaib910385/STALKERXUSERBOT")
 BRANCH = getenv("BRANCH", "master") #don't change
  
-STRING_SESSION1 = getenv("STRING_SESSION1", "")
+STRING_SESSION1 = getenv("STRING_SESSION1", "1BJWap1sBu5xqqGQzbScTlqoHAF7orhCEpBXUIL5hklAhi830CKhWRj99tbOWBFOM0JFz29YPPCNkJhM2A8ycpNvSH0JJO5H1Ocw84NgXiGM348ijDSHVi3XxK_FnPVDoItAuKZxL9sBlKcR3Uz0OJnIZJ7lFXbntx1DHhQE-mQ9sGJkxr1JqAzgfV3kghqvzig62sdley2w6rL1KLpraayVjDkZEpUMUut-i7nVLTWJaePgNYr19miiwq-6Jk0WgozQDE_r2x6TjHQUGjoz9Szhf5PLg_U702wM-g3zfv2Md6wkdI_sxaPEfeaTYVQXey4nISVil0BklmyLJCsElRfjsyINS2KY=")
 STRING_SESSION2 = getenv("STRING_SESSION2", "")
 STRING_SESSION3 = getenv("STRING_SESSION3", "")
 STRING_SESSION4 = getenv("STRING_SESSION4", "")
